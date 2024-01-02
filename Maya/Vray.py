48838fe1375847d731419707cc6b64c0ebba266e
chris-laptop <christian.gyldmark@copenhagenbombay.com>
2024-01-02 07:40:23 +0100

    setting up the base project

---
+++ Maya/Vray.py
@@ -0,0 +1,12 @@
+def run():
+    """
+    a great function
+    :return:
+    """
+    print(f"Running {__file__}")
+
+def print_vray():
+    print("vray")
+
+if __name__ == "__main__":
+    run()
---
+++ launcher.py
@@ -0,0 +1,10 @@
+"""
+This would be the launcher. It should open up and run any application needed
+"""
+import os
+
+def run():
+    print(f"Running {__file__}")
+
+if __name__ == "__main__":
+    run()
